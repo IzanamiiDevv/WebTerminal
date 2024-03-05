21dd47c99053861020e46ee77d1adca36e04baac
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-06 07:26:13 +0800

    again

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 
 namespace WebTerminal.Pages;
 
@@ -10,10 +11,11 @@ public class IndexModel : PageModel
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+        Console.WriteLine("Liggs");
     }
 
     public void OnGet()
     {
-
+        Console.WriteLine("Ligga");
     }
 }
