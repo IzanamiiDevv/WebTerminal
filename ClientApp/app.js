7fc21ed5f276fe5cfe9837ed8e44e0824a1a9ab3
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-06 23:14:22 +0800

    express

--- ClientApp/app.js
+++ ClientApp/app.js
@@ -1,1 +1,14 @@
-console.log("IDK")
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const public = path.join(__dirname,'public');
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(public,"index.html"));
+})
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`)
+})
