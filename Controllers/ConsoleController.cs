8951ca1027896e20b1526e48261e9791da379ad3
IzanamiiDevv <rafaeloli596@gmail.com>
2024-03-07 00:06:54 +0800

    wwww

---
+++ Controllers/ConsoleController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebTerminal.Controllers;
+
+[ApiController]
+[Route("/server")]
+public class ConsoleController : ControllerBase
+{
+    [HttpGet()]
+    public IActionResult GetMain()
+    {
+        string message = "Main Server EndPoint";
+        return Ok(new {Text = message});
+    }
+    [HttpGet("test")]
+    public IActionResult GetTest()
+    {
+        string message = "Hello From Server";
+        return Ok(new { Text = message });
+    }
+}
